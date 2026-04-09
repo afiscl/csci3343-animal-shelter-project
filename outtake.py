23c89dc9492c14cbd790afe6e5a419652e592cae
Alex Fiscal <jose.fiscal02@utrgv.edu>
2026-04-08 19:42:12 -0500

    Total adopt stats

--- outtake.py
+++ outtake.py
@@ -6,8 +6,30 @@ import matplotlib.pyplot as plt
 import statsmodels.api as sm
 
 outtakes = pd.read_csv("Austin_Animal_Center_Outcomes__10_01_2013_to_05_05_2025_.csv")
-print(outtakes['Outcome Type'])
-print(len(outtakes.index))
+# print(outtakes['Outcome Type'])
+# print('length of data wo filtering:')
+# print(len(outtakes.index))
 
 nonBlankOutComes = outtakes[outtakes['Outcome Type'].str.len() > 0]
+print('filtered out blank outcome types.. new length:')
 print(len(nonBlankOutComes.index))
+
+# print('all possible outcome types:')
+# print(nonBlankOutComes['Outcome Type'].unique())
+
+#'Transfer',
+#'Euthanasia'
+#'Adoption'
+#'Return to Owner'
+#'Died'
+#'Missing'
+#'Disposal'
+#'Relocate'
+#'Rto-Adopt'
+#'Stolen'
+#'Lost'
+
+totalAdopts = len(nonBlankOutComes[nonBlankOutComes['Outcome Type'] == 'Adoption']) + len(nonBlankOutComes[nonBlankOutComes['Outcome Type'] == 'Rto-Adopt'])
+print(totalAdopts / len(nonBlankOutComes.index))
+# nearly 49% adoption rate(?) in austin animal center 
+# 49% for our limited view of texas?
